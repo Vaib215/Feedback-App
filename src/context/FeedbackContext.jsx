5f6e4695bebc1f219536c0bfef1c0098c8088c4e
Vaibhav sisodiya <sisodiya.vaib215@gmail.com>
2022-05-06 06:25:24 +0000

    Deploy on heroku

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -12,13 +12,13 @@ export const FeedbackProvider = ({ children }) => {
     // Delete Feeback Function
     const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure you want to delete this?")) {
-            await fetch(`/feedback-app/feedback/${id}`,{method:'DELETE'})
+            await fetch(`https://3001-vaib215-feedbackapp-aigkla28xpe.ws-us43.gitpod.io/feedback/${id}`,{method:'DELETE'})
             setFeedback(feedback.filter(item => item.id !== id))
         }
     }
     // Add Feedback Function
     const addFeedback = async (newFeedback) => {
-        const response = await fetch(`/feedback-app/feedback`,{
+        const response = await fetch(`https://3001-vaib215-feedbackapp-aigkla28xpe.ws-us43.gitpod.io/feedback`,{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -37,7 +37,7 @@ export const FeedbackProvider = ({ children }) => {
         })
     }
     const updateFeedback = async (id,updItem) => {
-        const response = await fetch(`/feedback-app/feedback/${id}`,{
+        const response = await fetch(`https://3001-vaib215-feedbackapp-aigkla28xpe.ws-us43.gitpod.io/feedback/${id}`,{
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -52,7 +52,7 @@ export const FeedbackProvider = ({ children }) => {
     }, [])
     
     const fetchFeedback = async () => {
-        const response = await fetch(`/feedback-app/feedback?_sort=id&_order=desc`)
+        const response = await fetch(`https://3001-vaib215-feedbackapp-aigkla28xpe.ws-us43.gitpod.io/feedback?_sort=id&_order=desc`)
         const data = await response.json()
         setFeedback(data)
         setIsLoading(false)
